5c971e415cfb181ab687d1e113fe6340ee6853e4
nguyentran4896 <nguyentran4896@gmail.com>
2018-03-11 14:43:18 +0700

    Added part 2: calculate distance, added google map frame

--- Ex1/public/javascripts/latlg.js
+++ Ex1/public/javascripts/latlg.js
@@ -1,14 +1,50 @@
-$('#btn-submit').on('click', function (){
-    callGoogleApi();
-});
-$(document).on('keyup', function (e) {
-    if (e.which == 13) callGoogleApi();
+$('#btn-address-submit').on('click', function () {
+    latlongToAddress();
 });
 
-function callGoogleApi() {
-    $('.result h4').remove();
+$('#btn-2-points-submit').on('click', function () {
+    latlongToDistance();
+});
+
+var map;
+$(document).on('keyup', function (e) {
+    if (e.which == 13) latlongToAddress();
+});
+
+function latlongToAddress() {
+    $('.address-result h4').remove();
     $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + $('#latitude').val().trim() + ',' +
         $('#longtitude').val().trim() + '&key=AIzaSyDYsNsVJsF6jCGhCA85lojWX7xcT6x9VTo', function (res) {
-            $('.result').append('<h4>' + res.results[0].formatted_address + '</h4>');
+            $('.address-result').prepend('<h4>' + res.results[0].formatted_address + '</h4>');
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: res.results[0].geometry.location,
+                zoom: 18
+            });
         });
+}
+
+function latlongToDistance() {
+    $('.distance-result h4').remove();
+
+    var lat1 = $('#lat-1').val().trim();
+    var lon1 = $('#lon-1').val().trim();
+    var lat2 = $('#lat-2').val().trim();
+    var lon2 = $('#lon-2').val().trim();
+
+    $.post('/api/calculate_distance', {
+        lat1: lat1,
+        lon1: lon1,
+        lat2: lat2,
+        lon2: lon2
+    }, function (res) {
+        console.log(res)
+        $('.distance-result').append('<h4>Distance: ' + res.distance.toFixed(2) + ' km</h4>');
+    });
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
 }
--- Ex1/routes/index.js
+++ Ex1/routes/index.js
@@ -2,8 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Latlg App' });
 });
+
+router.post('/api/calculate_distance', function (req, res, next) {
+  var lat1 = req.body.lat1;
+  var lon1 = req.body.lon1;
+  var lat2 = req.body.lat2;
+  var lon2 = req.body.lon2;
+  res.send({ distance: getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) });
+});
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180)
+}
 
 module.exports = router;
